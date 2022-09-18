09e56c9672defe801d9938b1095b943e9f005433
Alex <alexander.cg.carlsson@gmail.com>
2022-09-19 01:02:05 +0200

    Constraints-changes to modify how the app looks on larger devices.

--- app/src/main/java/com/example/myapplication/RulesActivity.kt
+++ app/src/main/java/com/example/myapplication/RulesActivity.kt
@@ -4,25 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-/*
 
-class AnswerAcitivty : AppCompatActivity() {
-
-    lateinit var resultTextView : TextView
-    lateinit var layout : ConstraintLayout
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_answer_acitivty)
-
-        var test = findViewById<ConstraintLayout>(R.id.activityView)
-        resultTextView = findViewById(R.id.resultView)
-        val button = findViewById<Button>(R.id.backButton)
-        button.text = "Back"
-        button.setOnClickListener{
-            finish()
-        }
-
- */
 class RulesActivity : AppCompatActivity() {
 
     lateinit var rulesTitleView : TextView
