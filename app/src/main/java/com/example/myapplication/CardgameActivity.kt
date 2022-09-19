26ea8687d68bc41ab09a6c0d2bfdc526de3e6f60
Alex <alexander.cg.carlsson@gmail.com>
2022-09-19 11:53:21 +0200

    Added counters in every corner that will show the amount of points the players have

--- app/src/main/java/com/example/myapplication/CardgameActivity.kt
+++ app/src/main/java/com/example/myapplication/CardgameActivity.kt
@@ -11,23 +11,64 @@ class CardgameActivity : AppCompatActivity() {
     lateinit var playerTwoScore : TextView
     lateinit var playerThreeScore : TextView
     lateinit var playerFourScore : TextView
+    var scoreP1 = 0
+    var scoreP2 = 0
+    var scoreP3 = 0
+    var scoreP4 = 0
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cardgame_activity)
 
+        initiatePlayerScoreViews()
+        amountOfPlayers()
+
+
+
+
+    }
+
+    //Initierar de textviews som används för att visa poängställningen och knyter dem till
+    //scoreP1-scoreP4 för att visa poängen
+    fun initiatePlayerScoreViews() {
+
         playerOneScore = findViewById(R.id.counterOne)
+        playerOneScore.text = scoreP1.toString()
 
         playerTwoScore = findViewById(R.id.counterTwo)
-        playerTwoScore.visibility= View.INVISIBLE
+        playerTwoScore.text = scoreP2.toString()
 
         playerThreeScore = findViewById(R.id.counterThree)
-        playerThreeScore.visibility= View.INVISIBLE
+        playerThreeScore.text = scoreP3.toString()
 
         playerFourScore = findViewById(R.id.counterFour)
-        playerFourScore.visibility= View.INVISIBLE
+        playerFourScore.text = scoreP4.toString()
+    }
 
 
+    fun amountOfPlayers() {
+        val numOfPlayers = intent.getIntExtra("numOfPlayers", 0)
+
+        if (numOfPlayers != 0){
+            when (numOfPlayers.toString().toInt()) {
+                1 -> {
+                    playerTwoScore.visibility = View.INVISIBLE
+                    playerThreeScore.visibility = View.INVISIBLE
+                    playerFourScore.visibility = View.INVISIBLE
+                }
+                2 -> {
+                    playerThreeScore.visibility = View.INVISIBLE
+                    playerFourScore.visibility = View.INVISIBLE
+                }
+                3 -> {
+                    playerFourScore.visibility = View.INVISIBLE
+
+                }
+        }
+
+        }
     }
 }
 
