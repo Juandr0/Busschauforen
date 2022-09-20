c8efe94ead4f3c04e101e54b9a4242302130c1a3
Alex <alexander.cg.carlsson@gmail.com>
2022-09-20 14:25:29 +0200

    added player & card class, deckofcard list, list that is initiated with all cards

--- app/src/main/java/com/example/myapplication/CardgameActivity.kt
+++ app/src/main/java/com/example/myapplication/CardgameActivity.kt
@@ -2,8 +2,20 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
+
+
+class Player (var name : String){
+
+}
+
+
+class Card (var name : String, var isFaceCard : Boolean = false) {
+}
+
+
 
 class CardgameActivity : AppCompatActivity() {
 
@@ -18,15 +30,18 @@ class CardgameActivity : AppCompatActivity() {
 
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cardgame_activity)
 
+        var deckOfCards = initiateDeckOfCards()
+        deckOfCards.shuffle()
+
         initiatePlayerScoreViews()
         amountOfPlayers()
-
-
-
+        asignCardsToPyramid(deckOfCards)
+        
 
     }
 
@@ -69,6 +84,80 @@ class CardgameActivity : AppCompatActivity() {
         }
 
         }
+    }
+
+    fun asignCardsToPyramid(cardList : MutableList<Card>){
+
+    }
+
+    fun initiateDeckOfCards () : MutableList<Card>{
+
+        var cardList = mutableListOf<Card>()
+
+        //Spader
+        cardList.add(Card("ace_of_spades"))
+        cardList.add(Card("two_of_spades"))
+        cardList.add(Card("three_of_spades"))
+        cardList.add(Card("four_of_spades"))
+        cardList.add(Card("five_of_spades"))
+        cardList.add(Card("six_of_spades"))
+        cardList.add(Card("seven_of_spades"))
+        cardList.add(Card("eight_of_spades"))
+        cardList.add(Card("nine_of_spades"))
+        cardList.add(Card("ten_of_spades"))
+        cardList.add(Card("knight_of_spades", isFaceCard = true))
+        cardList.add(Card("queen_of_spades", isFaceCard = true))
+        cardList.add(Card("king_of_spades", isFaceCard = true))
+
+        //Hjärter
+        cardList.add(Card("ace_of_hearts"))
+        cardList.add(Card("two_of_hearts"))
+        cardList.add(Card("three_of_hearts"))
+        cardList.add(Card("four_of_hearts"))
+        cardList.add(Card("five_of_hearts"))
+        cardList.add(Card("six_of_hearts"))
+        cardList.add(Card("seven_of_hearts"))
+        cardList.add(Card("eight_of_hearts"))
+        cardList.add(Card("nine_of_hearts"))
+        cardList.add(Card("ten_of_hearts"))
+        cardList.add(Card("knight_of_hearts", isFaceCard = true))
+        cardList.add(Card("queen_of_hearts", isFaceCard = true))
+        cardList.add(Card("king_of_hearts", isFaceCard = true))
+
+        //Klöver
+
+        cardList.add(Card("ace_of_clubs"))
+        cardList.add(Card("two_of_clubs"))
+        cardList.add(Card("three_of_clubs"))
+        cardList.add(Card("four_of_clubs"))
+        cardList.add(Card("five_of_clubs"))
+        cardList.add(Card("six_of_clubs"))
+        cardList.add(Card("seven_of_clubs"))
+        cardList.add(Card("eight_of_clubs"))
+        cardList.add(Card("nine_of_clubs"))
+        cardList.add(Card("ten_of_clubs"))
+        cardList.add(Card("knight_of_clubs", isFaceCard = true))
+        cardList.add(Card("queen_of_clubs", isFaceCard = true))
+        cardList.add(Card("king_of_clubs", isFaceCard = true))
+
+        //Ruter
+        cardList.add(Card("ace_of_diamonds"))
+        cardList.add(Card("two_of_diamonds"))
+        cardList.add(Card("three_of_diamonds"))
+        cardList.add(Card("four_of_diamonds"))
+        cardList.add(Card("five_of_diamonds"))
+        cardList.add(Card("six_of_diamonds"))
+        cardList.add(Card("seven_of_diamonds"))
+        cardList.add(Card("eight_of_diamonds"))
+        cardList.add(Card("nine_of_diamonds"))
+        cardList.add(Card("ten_of_diamonds"))
+        cardList.add(Card("knight_of_diamonds", isFaceCard = true))
+        cardList.add(Card("queen_of_diamonds", isFaceCard = true))
+        cardList.add(Card("king_of_diamonds", isFaceCard = true))
+
+
+
+    return cardList
     }
 }
 
